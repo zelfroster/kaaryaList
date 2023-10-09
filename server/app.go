4f53e1665581b3746a077ec99193b2437eff0c6e
zelfroster <sumitkumarsoni123@gmail.com>
2023-10-09 14:44:16 +0530

    feat: add more routes

--- server/app.go
+++ server/app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -25,12 +26,15 @@ func (app *App) Initialise(USER string, DBNAME string, PORT int) error {
     return err
   }
 
+  // Ping the db to check connection (not necessary)
   err = app.DB.Ping()
   if err != nil {
     return err
   }
 
+  // router to handle routes
   app.Router = mux.NewRouter().StrictSlash(true)
+
   app.handleRoutes()
   return nil
 }
@@ -56,11 +60,66 @@ func (app *App) getTasks(rw http.ResponseWriter, req *http.Request) {
   tasks, err := getTasks(app.DB)
   if err != nil {
     sendError(rw, http.StatusInternalServerError, err.Error())
+    return
   }
-  log.Println(tasks)
   sendResponse(rw, http.StatusOK, tasks)
+}
+
+func (app *App) getTask(rw http.ResponseWriter, req *http.Request) {
+  // get the id from the route accessed e.g. for route /getTask/1 , id will be 1
+  varMap := mux.Vars(req) 
+  key, err := strconv.Atoi(varMap["id"])
+  if err != nil {
+    sendError(rw, http.StatusBadRequest, "invalid task id")
+    return
+  }
+
+  task := Task{ID: key}
+
+  err = task.getTask(app.DB)
+  if err != nil {
+    sendError(rw, http.StatusInternalServerError, err.Error())
+    return
+  }
+  sendResponse(rw, http.StatusOK, task)
+}
+
+func (app *App) createTask(rw http.ResponseWriter, req *http.Request) {
+  var task Task
+  // decode data coming in request and assign to task
+  json.NewDecoder(req.Body).Decode(&task)
+
+  err := task.createTask(app.DB)
+  if err != nil {
+    sendError(rw, http.StatusInternalServerError, err.Error())
+    return
+  }
+  sendResponse(rw, http.StatusOK, task)
+}
+
+func (app *App) updateTask(rw http.ResponseWriter, req *http.Request) {
+  varMap := mux.Vars(req)
+  key, err := strconv.Atoi(varMap["id"])
+  if err != nil {
+    sendError(rw, http.StatusBadRequest, "invalid task id")
+    return
+  }
+
+  task := Task{ID: key}
+
+  json.NewDecoder(req.Body).Decode(&task)
+
+  err = task.updateTask(app.DB)
+  if err != nil {
+    sendError(rw, http.StatusInternalServerError, err.Error())
+    return
+  }
+  sendResponse(rw, http.StatusOK, task)
 }
 
 func (app *App) handleRoutes() {
   app.Router.HandleFunc("/getTasks", app.getTasks).Methods("GET")
+  app.Router.HandleFunc("/getTask/{id}", app.getTask).Methods("GET")
+  app.Router.HandleFunc("/createTask", app.createTask).Methods("POST")
+  app.Router.HandleFunc("/updateTask/{id}", app.updateTask).Methods("PUT")
 }
--- server/model.go
+++ server/model.go
@@ -2,31 +2,80 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type Task struct {
-  ID string `json:"id"`
-  Name string  `json:"name"`
-  IsComplete bool  `json:"isComplete"`
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	IsComplete bool   `json:"isComplete"`
 }
 
 func getTasks(db *sql.DB) ([]Task, error) {
-  queryString := "SELECT id, name, is_complete FROM tasks"
-  rows, err := db.Query(queryString)
-  if err != nil {
-    return nil, err
-  }
+	queryString := "SELECT id, name, is_complete FROM tasks"
+	rows, err := db.Query(queryString)
+	if err != nil {
+		return nil, err
+	}
 
-  tasks := []Task{}
+	// array of Tasks
+	tasks := []Task{}
 
-  for rows.Next() {
-    var task Task
-    err := rows.Scan(&task.ID, &task.Name, &task.IsComplete)
-    if err != nil {
-      return nil, err
-    }
-    tasks = append(tasks, task)
-  }
+	// Loop over the rows returned after querying the database
+	for rows.Next() {
+		var task Task
+		// store the values in task
+		err := rows.Scan(&task.ID, &task.Name, &task.IsComplete)
+		if err != nil {
+			return nil, err
+		}
+		// append to array
+		tasks = append(tasks, task)
+	}
 
-  return tasks, nil
+	return tasks, nil
+}
+
+func (task *Task) getTask(db *sql.DB) error {
+	queryString := fmt.Sprintf("SELECT name, is_complete FROM tasks WHERE id=%v", task.ID)
+	row := db.QueryRow(queryString)
+
+	err := row.Scan(&task.Name, &task.IsComplete)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (task *Task) createTask(db *sql.DB) error {
+	queryString := fmt.Sprintf("INSERT INTO tasks (name) VALUES ('%v')", task.Name)
+	res, err := db.Exec(queryString)
+	if err != nil {
+		return err
+	}
+
+	// get the Id of the newly created Task
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	task.ID = int(id)
+	return nil
+}
+
+func (task *Task) updateTask(db *sql.DB) error {
+	var queryString string
+	if task.Name == "" {
+		queryString = fmt.Sprintf("UPDATE tasks SET name='%v', is_complete=%v WHERE id=%v RETURNING *", task.Name, task.IsComplete, task.ID)
+	} else {
+		queryString = fmt.Sprintf("UPDATE tasks SET name='%v' WHERE id=%v RETURNING *", task.Name, task.ID)
+	}
+	row := db.QueryRow(queryString)
+	err := row.Scan(&task.ID, &task.Name, &task.IsComplete)
+	if err != nil {
+		return err
+	}
+	return nil
 }
