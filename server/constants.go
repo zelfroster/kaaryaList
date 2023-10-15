08364491847aace833df6146815732ab2bdd0738
zelfroster <sumitkumarsoni123@gmail.com>
2023-10-15 21:21:25 +0530

    feat: add environment variables using godotenv

--- server/constants.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const HOST string = "localhost"
-const USER string = "zel"
-const PORT int = 5432
-const PASSWORD string = ""
-const DBNAME string = "kaaryaList"
--- server/main.go
+++ server/main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+  // Load Environment Variables
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  user := os.Getenv("USER")
+  dbname := os.Getenv("DBNAME")
+  port, err := strconv.Atoi(os.Getenv("PORT"))
+  if err != nil {
+    log.Fatal("Error converting string to integer: ", err)
+  }
+
+  // Initialise App
   app := App{}
 
-  err := app.Initialise(USER, DBNAME, PORT)
+  err = app.Initialise(user, dbname, port)
   if err != nil {
     log.Fatal(err)
   }
